ea8cebb5427f580e2cc0f56cb20a814863553e9c
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-11 15:25:27 -0700

    testing

--- README.md
+++ README.md
@@ -42,56 +42,12 @@ $ chmod +x run_main.sh
 $ ./run_main.sh
 ```
 - Enter any information for which you are prompted.
-- After script has finished, update the oauth providers to accept login requests from the new URL.
+- After script has finished, update the oauth providers to accept login requests from the new URL, which should look something like this:
+http://ec2-XX-XX-XX-XX.us-west-2.compute.amazonaws.com/ with the XXs being your VPS ip address.
 - Enjoy the app.
 
 Pre-reqs:
-GIT must be installed on the host system. The user must have sudo access on host system.
-•	Download and put RSA key file in ~/.ssh/
-•	Change its file permissions with chmod 600 ~/.ssh/udacity_key.rsa
-•	SSH into the VPS with ssh –i ~/.ssh/udacity_key.rsa root@52.36.126.254
-•	Install git with apt-get install git-all
-•	Clone repo with git clone https://github.com/shteeven/vps-setup.git
-•	Change permission of setup file with chmod
-•	Create user ‘grader’ with adduser grader
-•	Give sudo permissions with mkdir ~/etc/sudoers.d/graders
-•	Log into ‘grader’ with su – grader
-•	Update all system packages with sudo apt-get update
-•	Upgrade all system packages with sudo apt-get upgrade
-•	Remove unused packages with sudo apt-get autoremove
-•	Install system-wide requirements with
-o	sudo apt-get install git-all
-o	git clone
 
 
-All depencies for the frontend are either contained within the repo or have links to their CDN. For the backend, you must first install [VirtualBox](https://www.virtualbox.org/), then [Vagrant](https://www.vagrantup.com/). Once these requirements are fulfilled, open terminal. From terminal, `cd` into Catalog Apps directory:
-```sh
-$ cd /catalog-app-master
-```
-Run vagrant up and wait for the virtual environment to be installed:
-```sh
-$ vagrant up
-```
-SSH into the vagrant environment:
-```sh
-$ vagrant ssh
-```
-Once in, cd to host folder:
-```sh
-$ cd ../../vagrant
-```
-Then run application.py to start the server:
-```sh
-$ python application.py
-```
-After this, the application is ready to use. In your browser, go to `localhost:8000`
 
-## Known issues
 
-There are many, but having the State 'refresh' when a new param is introduced is on the top of my list of fixes. The ability to edit profile info: coming soon.
-
-## Contributors
-
-Some code has been taken an modified from online tutorials:
-Login services courtesy of Gert Hengeveld at [Medium](https://medium.com/opinionated-angularjs/techniques-for-authentication-in-angularjs-applications-7bbf0346acec)
-Assistance in writing HTML and css styles provided by [Neven Hadjer](https://github.com/nevenhajder)
--- run_main.sh
+++ run_main.sh
@@ -4,121 +4,122 @@
 # Get data from user
 ################################
 
-echo "Please enter the git repo URL: "
-
-default_git_repo="https://github.com/shteeven/catalog-app.git"
-echo "Please enter git repo URL: MUST BE SAME AS REPO's NAME!!! ($default_git_repo)"
-read input_git_repo
-[[ $input_git_repo = '' ]] && git_repo="$default_git_repo" || git_repo="$input_git_repo"
-echo "You entered: $git_repo"
-
-default_app_name="catalog-app"
-echo "Please enter app name: MUST BE SAME AS REPO's NAME!!! ($default_app_name)"
-read input_app_name
-[[ $input_app_name = '' ]] && app_name="$default_app_name" || app_name="$input_app_name"
-echo "You entered: $app_name"
-
-default_username="grader"
-echo "Please enter username: ($default_username)"
-read input_username
-[[ $input_username = '' ]] && username="$default_username" || username="$input_username"
-echo "You entered: $username"
-
-default_port="2200"
-echo "Please enter desired ssh port: ($default_port)"
-read input_port
-[[ $input_port = '' ]] && ssh_port="$default_port" || ssh_port="$input_port"
-echo "You entered: $ssh_port"
-
-################################
-# Create new user and give appropriate permissions
-################################
-# create user
-adduser ${username}
-# give sudo access
-cat > /etc/sudoers.d/${username} << EOF1
-$username ALL=(ALL) NOPASSWD: ALL
-EOF1
-# switch users
-#su - ${username}
-echo "still running"
-
-################################
-# Update and Upgrade current packages
-################################
-apt-get update
-apt-get upgrade
-
-# for admin work later
-apt-get install finger
-
-################################
-# Update timezone
-################################
-timedatectl set-timezone UTC
-
-################################
-# Update port number and set up firewall
-################################
-regex='^Port 22$'
-file="/etc/ssh/sshd_config"
-
-if [ -f "$file" ] ; then
-	sed -i.bak "s|${regex}|Port ${ssh_port}|" $file
-else
-	echo "/etc/ssh/sshd_config could not be found."
-	echo "Please manually change Port 22 to desired number, then comment out all commands above and try running again."
-fi
-
-# block all ports by default
-ufw default deny incoming
-ufw default allow outgoing
-
-# open needed ports
-ufw allow ssh
-ufw allow ${ssh_port}/tcp
-ufw allow www
-ufw enable
-
-################################
-# Install apache and prepare server for application
-################################
-apt-get install apache2
-apt-get install postgresql postgresql-contrib
-# is this necessary???
-apt-get install memcached
-
-apt-get install libapache2-mod-wsgi python-dev python-pip build-essential
-pip install --upgrade pip
-
-conf_regex="application_name"
-conf_reg_username="user_name"
-conf_file="/root/vps-setup/000-default.conf"
-if [ -f "$conf_file" ] ; then
-	sed -i.bak "s|${conf_reg_username}|${username}|g" $conf_file
-	sed -i.bak "s|${conf_regex}|${app_name}|g" $conf_file
-fi
-cat $conf_file
-
-cp -f $conf_file /etc/apache2/sites-enabled/
-
-################################
-# Install application and dependencies
-################################
-app_dir="/var/www"
-cd ${app_dir}
-git clone ${git_repo}
-cd ${app_dir}/$app_name/vps/
-chmod +x run_vps.sh
-
-./run_vps.sh
-
-################################
-# Set up PSQL
-################################
+#echo "Please enter the git repo URL: "
+#
+#default_git_repo="https://github.com/shteeven/catalog-app.git"
+#echo "Please enter git repo URL: MUST BE SAME AS REPO's NAME!!! ($default_git_repo)"
+#read input_git_repo
+#[[ $input_git_repo = '' ]] && git_repo="$default_git_repo" || git_repo="$input_git_repo"
+#echo "You entered: $git_repo"
+#
+#default_app_name="catalog-app"
+#echo "Please enter app name: MUST BE SAME AS REPO's NAME!!! ($default_app_name)"
+#read input_app_name
+#[[ $input_app_name = '' ]] && app_name="$default_app_name" || app_name="$input_app_name"
+#echo "You entered: $app_name"
+#
+#default_username="grader"
+#echo "Please enter username: ($default_username)"
+#read input_username
+#[[ $input_username = '' ]] && username="$default_username" || username="$input_username"
+#echo "You entered: $username"
+#
+#default_port="2200"
+#echo "Please enter desired ssh port: ($default_port)"
+#read input_port
+#[[ $input_port = '' ]] && ssh_port="$default_port" || ssh_port="$input_port"
+#echo "You entered: $ssh_port"
+#
+#################################
+## Create new user and give appropriate permissions
+#################################
+## create user
+#adduser ${username}
+## give sudo access
+#cat > /etc/sudoers.d/${username} << EOF1
+#$username ALL=(ALL) NOPASSWD: ALL
+#EOF1
+## switch users
+##su - ${username}
+#echo "still running"
+#
+#################################
+## Update and Upgrade current packages
+#################################
+#apt-get update
+#apt-get upgrade
+#
+## for admin work later
+#apt-get install finger
+#
+#################################
+## Update timezone
+#################################
+#timedatectl set-timezone UTC
+#
+#################################
+## Update port number and set up firewall
+#################################
+#regex='^Port 22$'
+#file="/etc/ssh/sshd_config"
+#
+#if [ -f "$file" ] ; then
+#	sed -i.bak "s|${regex}|Port ${ssh_port}|" $file
+#else
+#	echo "/etc/ssh/sshd_config could not be found."
+#	echo "Please manually change Port 22 to desired number, then comment out all commands above and try running again."
+#fi
+#
+## block all ports by default
+#ufw default deny incoming
+#ufw default allow outgoing
+#
+## open needed ports
+#ufw allow ssh
+#ufw allow ${ssh_port}/tcp
+#ufw allow ntp
+#ufw allow www
+#ufw enable
+#
+#################################
+## Install apache and prepare server for application
+#################################
+#apt-get install apache2
+#apt-get install postgresql postgresql-contrib
+## is this necessary???
+#apt-get install memcached
+#
+#apt-get install libapache2-mod-wsgi python-dev python-pip build-essential
+#pip install --upgrade pip
+#
+#conf_regex="application_name"
+#conf_reg_username="user_name"
+#conf_file="/root/vps-setup/000-default.conf"
+#if [ -f "$conf_file" ] ; then
+#	sed -i.bak "s|${conf_reg_username}|${username}|g" $conf_file
+#	sed -i.bak "s|${conf_regex}|${app_name}|g" $conf_file
+#fi
+#cat $conf_file
+#
+#cp -f $conf_file /etc/apache2/sites-enabled/
+#
+#################################
+## Install application and dependencies
+#################################
+#app_dir="/var/www"
+#cd ${app_dir}
+#git clone ${git_repo}
+#cd ${app_dir}/$app_name/vps/
+#chmod +x run_vps.sh
+#
+#./run_vps.sh
+#
+#################################
+## Set up PSQL
+#################################
 
 sudo -u postgres psql -c "CREATE USER catalog WITH PASSWORD 'catalog' CREATEDB LOGIN;"
-sudo -u postgres psql -c "CREATE ROLE catalog WITH CREATEDB;"
+
 
 service apache2 restart
 
