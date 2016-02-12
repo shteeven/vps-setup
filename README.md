3351ba4eae89643248408416c0da709a1767fa32
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-12 13:00:38 -0700

    add cron job and status monitoring

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 ## Synopsis
 
-A fast, simple and secure set-up for running your Flask application on a vps. This set up assumes you are on a Unix system and have a VPS set up with Amazon, Heroku, or other like service that runs an Ubuntu 14.04 system. It will work with others, but this is the only tested version.
+THIS FILE IS DESIGNED FOR A UDACITY NANODEGREE PROJECT. It can be modded to fit many others; use it as you wish, but know that some steps have been taken care of by Udacity, and are not covered in this README.
 
-This script still requires to make it more universal, but for now, it works with my catalog-app
+A fast, simple and secure set-up for running your Flask application on a vps. This set up assumes you are on a Unix system (only tested on Mac OSX) and have a VPS set up with Amazon, Heroku, or other like service that runs an Ubuntu 14.04 system. It will work with others, but this is the only tested version.
+
+This script still requires work to make it more universal, but for now, it works with my catalog-app. There are some steps that are project specific, and I have placed those in a VPS folder within that specific project. This keeps things more modular; you can use the catalog-app VPS folder as an example, [here][1].
 
 ## Installation
-
+- Open two terminals: one for local and one for later work with the vps.
+#### LOCAL TERMINAL
+- Create a folder to store keys: '~/.ssh/vps_rsa':
+```sh
+$ touch ~/.ssh/vps_rsa
+```
+- Generate an SSH key locally and save it to '/Users/YOUR_HOME/.ssh/vps_rsa' (enter a password if you wish):
+```sh
+$ ssh-keygen
+```
+-  View the contents of '~/.ssh/vps_rsa':
+```sh
+$ cat ~/.ssh/vps_rsa.pub
+```
+- Leave this window open and copy and paste the vps_rsa contents when prompted by the script.
+#### VPS TERMINAL
 - Download and put RSA key file in ~/.ssh/:
 ```sh
 $ mv ~/Downloads/udacity_key.rsa ~/.ssh/
@@ -44,10 +61,7 @@ $ ./run_main.sh
 - Enter any information for which you are prompted.
 - After script has finished, update the oauth providers to accept login requests from the new URL, which should look something like this:
 http://ec2-XX-XX-XX-XX.us-west-2.compute.amazonaws.com/ with the XXs being your VPS ip address.
+- Once finished, you will have to login as the newly created user from the port chosen; it should look similar to this: `ssh –i ~/.ssh/vps_rsa grader@XX.XX.XX.XXX -p 2200`
 - Enjoy the app.
 
-Pre-reqs:
-
-
-
-
+[1]: https://github.com/shteeven/catalog-app/tree/master/vps
--- run_main.sh
+++ run_main.sh
@@ -3,6 +3,10 @@
 ################################
 # Get data from user
 ################################
+echo "Enter the contents of the public key you just generated:"
+echo "If you fail to enter the info correctly, you must start over."
+read rsa_pub_key
+echo "You entered: $rsa_pub_key"
 
 default_git_repo="https://github.com/shteeven/catalog-app.git"
 echo "Please enter git repo URL: MUST BE SAME AS REPO's NAME!!! ($default_git_repo)"
@@ -21,6 +25,11 @@ echo "Please enter username: ($default_username)"
 read input_username
 [[ $input_username = '' ]] && username="$default_username" || username="$input_username"
 echo "You entered: $username"
+
+echo "Please enter your server's public IP:"
+echo "THIS IS A MUST!!!!:"
+read public_ip
+echo "You entered: $public_ip"
 
 default_port="2200"
 echo "Please enter desired ssh port: ($default_port)"
@@ -48,9 +57,22 @@ apt-get upgrade
 apt-get install finger
 
 ################################
-# Update timezone
+# Update timezone and setup CRON jobs
 ################################
+# set timezone
 timedatectl set-timezone UTC
+# install cron job handler: probably already on here
+apt-get install cron
+
+# copy cron tab to temp file
+crontab -l > mycron
+# append cron task; update system packages weekly
+echo "@weekly sudo apt-get update" >> mycron
+echo "@weekly sudo apt-get upgrade" >> mycron
+# install new cron file
+crontab mycron
+# delete temp file
+rm mycron
 
 ################################
 # Update port number and set up firewall
@@ -59,6 +81,8 @@ regex='^Port 22$'
 regex_rootlogin='^PermitRootLogin .*$'
 file="/etc/ssh/sshd_config"
 
+# copy public key from root to the new user
+# if you want something actually secure, do it manually; go to here:
 # change ssh port and disable root user login
 if [ -f "$file" ] ; then
 	sed -i.bak "s|${regex}|Port ${ssh_port}|" $file
@@ -85,16 +109,12 @@ apt-get install fail2ban
 
 # copy limit rates
 cp /root/vps-setup/files/nginx.conf /etc/nginx/conf.d/nginx.conf
-
 # copy fail2ban triggers
 cp /root/vps-setup/files/jail.local /etc/fail2ban/jail.local
-
 # copy ban actions
 cp /root/vps-setup/files/ufw.conf /etc/fail2ban/action.d/ufw.conf
-
 # copy filter limits
 cp /root/vps-setup/files/filter-nginx-req-limit.conf /etc/fail2ban/filter.d/nginx-req-limit.conf
-
 # copy jail limits
 cp /root/vps-setup/files/jail-nginx-req-limit.conf /etc/fail2ban/jail.d/nginx-req-limit.conf
 
@@ -107,12 +127,12 @@ service nginx reload
 ################################
 apt-get install apache2
 apt-get install postgresql postgresql-contrib
-# is this necessary???
+# TODO: is this necessary???
 apt-get install memcached
-
 apt-get install libapache2-mod-wsgi python-dev python-pip build-essential
 pip install --upgrade pip
 
+# configure Apache to look for the wsgi file in your project directory
 conf_regex="application_name"
 conf_reg_username="user_name"
 conf_file="/root/vps-setup/files/000-default.conf"
@@ -120,9 +140,35 @@ if [ -f "$conf_file" ] ; then
 	sed -i.bak "s|${conf_reg_username}|${username}|g" $conf_file
 	sed -i.bak "s|${conf_regex}|${app_name}|g" $conf_file
 fi
-cat $conf_file
-
+# copy newly modded file and replace old config file
 cp -f $conf_file /etc/apache2/sites-enabled/
+
+# enable Apache monitoring
+a2enmod status
+# include the new config file
+echo "Include /etc/apache2/httpd.conf" >> /etc/apache2/apache2.conf
+# insert user input and copy new config file
+ip_regex="new_public_ip"
+httpd_conf_file="/root/vps-setup/files/httpd.conf"
+if [ -f "$httpd_conf_file" ] ; then
+	sed -i.bak "s|${ip_regex}|${public_ip}|" $conf_file
+fi
+cp /root/vps-setup/files/httpd.conf /etc/apache2/httpd.conf
+
+# Config agent url to access status and add password protection
+agent_regex="^apache_status_url.*$"
+agent_conf_file="/etc/sd-agent/config.cfg"
+agent_new_line="apache_status_url: http://${public_ip}/server-status?auto"
+# replace line if exists, otherwise, append new line to file
+if grep "$agent_conf_file" $agent_regex > /dev/null
+then
+   sed -i.bak "s|${agent_regex}|${agent_new_line}|" $agent_conf_file
+else
+   echo ${agent_new_line} >> $agent_conf_file
+fi
+# set password and username to the new created user's name
+echo "apache_status_user: ${username}" >> $agent_conf_file
+echo "apache_status_pass:${username}" >> $agent_conf_file
 
 ################################
 # Install application and dependencies
@@ -138,17 +184,28 @@ chmod +x run_vps.sh
 ################################
 # Set up PSQL
 ################################
-
 sudo -u postgres psql -c "CREATE USER catalog WITH PASSWORD 'catalog' CREATEDB LOGIN;"
 sudo -u postgres psql -c "REVOKE CONNECT ON DATABASE catalog FROM PUBLIC;"
 sudo -u postgres psql -c "GRANT CONNECT ON DATABASE catalog TO catalog;"
 
+################################
+# Final security set up
+################################
+# run commands as the newly created user
+# this is required for SSH to accept the files as authentic
+sudo -u ${username} bash << EOF
+cd /home/${username}/
+mkdir .ssh
+cd .ssh
+echo ${rsa_pub_key} >> authorized_keys
+cd ..
+chmod 700 .ssh
+chmod 644 .ssh/authorized_keys
+EOF
+
+echo "FROM NOW ON, YOU MUST LOGIN AS ${username}"
+echo "ssh -i ~/.ssh/YOUR_LOCAL_KEY.rsa ${username}@XX.XX.XXX.XXX -p 2200"
+
 service apache2 restart
-
-echo "YOU WILL HAVE TO LOGIN AS ${username}"
-echo "ssh -i ~/.ssh/udacity_key.rsa ${username}@XX.XX.XXX.XXX"
-sshd restart
-
-
-
-
+service ssh restart
+reboot
