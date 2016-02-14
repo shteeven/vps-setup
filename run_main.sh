76330f28213a4330a399959657898b7d65e3e26c
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-14 11:58:40 -0700

    Change sudo permissions of new user to require a password.

--- run_main.sh
+++ run_main.sh
@@ -45,7 +45,7 @@ echo "You entered: $ssh_port"
 adduser ${username}
 # give sudo access
 cat > /etc/sudoers.d/${username} << EOF1
-$username ALL=(ALL) NOPASSWD: ALL
+$username ALL=(ALL) ALL
 EOF1
 
 ################################
@@ -68,8 +68,8 @@ apt-get install cron
 # copy cron tab to temp file
 crontab -l > mycron
 # append cron task; update system packages weekly
-echo "@weekly sudo apt-get update" >> mycron
-echo "@weekly sudo apt-get upgrade" >> mycron
+echo "@weekly apt-get update" >> mycron
+echo "@weekly apt-get upgrade" >> mycron
 # install new cron file
 crontab mycron
 # delete temp file
@@ -80,6 +80,7 @@ rm mycron
 ################################
 regex='^Port 22$'
 regex_rootlogin='^PermitRootLogin .*$'
+regex_password='^PasswordAuthentication .*$'
 file="/etc/ssh/sshd_config"
 
 # copy public key from root to the new user
@@ -139,43 +140,6 @@ cp -f $conf_file /etc/apache2/sites-enabled/
 
 # enable Apache monitoring
 a2enmod status
-#############################
-# Unable to get monitoring to work appropriately
-#############################
-
-## include the new config file
-#echo "Include /etc/apache2/httpd.conf" >> /etc/apache2/apache2.conf
-## insert user input and copy new config file; problems with use of tilde here, so used cd instead of paths
-#
-#ip_regex="new_public_ip"
-#cd ~/vps-setup/files/
-#if [ -f "httpd.conf" ] ; then
-#	echo "HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-#	sed -i.bak "s|${ip_regex}|${public_ip}|" httpd.conf
-#fi
-#cp httpd.conf /etc/apache2/httpd.conf
-#cd ~/
-#
-## Config agent url to access status and add password protection
-#agent_regex="^apache_status_url.*$"
-#agent_conf_file="/etc/sd-agent/config.cfg"
-#agent_new_line="apache_status_url: http://${public_ip}/server-status?auto"
-## replace line if exists, otherwise, append new line to file
-#if grep $agent_regex $agent_conf_file > /dev/null
-#then
-#	sed -i.bak "s|${agent_regex}|${agent_new_line}|" $agent_conf_file
-#else
-#	if [ -f "$agent_conf_file" ] ; then
-#   		echo ${agent_new_line} >> $agent_conf_file
-#   	else
-#   		mkdir /etc/sd-agent/
-#   		touch ${agent_conf_file}
-#   		echo ${agent_new_line} >> $agent_conf_file
-#   	fi
-#fi
-## set password and username to the new created user's name
-#echo "apache_status_user: ${username}" >> $agent_conf_file
-#echo "apache_status_pass: ${username}" >> $agent_conf_file
 
 ################################
 # Install application and dependencies
